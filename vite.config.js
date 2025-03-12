75152fd654838cdc01687e60aaf0576c99020e81
Md. Sirazul Islam <sirazulislam3734@gmail.com>
2025-03-13 02:25:26 +0600

    added vite.config and update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import daisyui from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), daisyui()],
+  plugins: [react(), daisyui(), tailwindcss()],
 });
