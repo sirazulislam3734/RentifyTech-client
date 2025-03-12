64d7369c4ca41a15572c28f3ae32bfb9d6825af3
Md. Sirazul Islam <sirazulislam3734@gmail.com>
2025-03-13 01:00:38 +0600

    added components and created folder and file

--- src/Khaled.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Khaled() {
-  return (
-    <div>Khaled- Mahi</div>
-  )
-}
-
-export default Khaled
---
+++ src/Pages/signIn/SignIn.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            This is SignIn Page
+        </div>
+    );
+};
+
+export default SignIn;
