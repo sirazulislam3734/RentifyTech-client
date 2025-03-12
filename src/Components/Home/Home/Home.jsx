11e89ddfd816c87fab4a1153be6b1acfae32208b
Md. Sirazul Islam <sirazulislam3734@gmail.com>
2025-03-13 01:35:33 +0600

    Your commit message

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import Khaled from "../../../Khaled"
 
 const Home = () => {
   return (
     <div>
-      <h1 className="bg-red-500">this is home</h1>
+      <h1 className="bg-red-500">this is home </h1>
+      <Khaled></Khaled>
     </div>
   );
 };
