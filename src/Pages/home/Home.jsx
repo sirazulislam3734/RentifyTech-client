da01a08a6c298016432dfe013b15f0dd86ee69e1
PIASH <zmnobab1999@gmail.com>
2025-03-13 13:57:49 +0600

    featured products section created

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../../Components/Navbar";
 import Testimonials from "./testimonials/Testimonials";
 import Footer from "../../Components/Footer";
+import FeaturedProducts from "./FeaturedProducts/FeaturedProducts";
 
 const Home = () => {
   return (
     <div className="">
       <Navbar></Navbar>
       <Testimonials />
+      <FeaturedProducts></FeaturedProducts>
     <Footer />
     
     </div>
