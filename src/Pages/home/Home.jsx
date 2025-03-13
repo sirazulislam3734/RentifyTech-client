1ecc333c0b7c9a53215ba551f88e478db2bfdbe1
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2025-03-13 09:46:17 +0600

    testimonials section created

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../Components/Navbar';
+import Testimonials from './testimonials/Testimonials';
 
 const Home = () => {
-    return (
-        <div>
-         <Navbar></Navbar>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Testimonials />
+    </div>
+  );
 };
 
 export default Home;
