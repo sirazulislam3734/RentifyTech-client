16bafbde4beaac2636d843cbaadebbfbab45bf22
Md. Sirazul Islam <sirazulislam3734@gmail.com>
2025-03-13 19:51:38 +0600

    update navbar logo

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <a className="btn btn-ghost text-xl">RentifyTech</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
